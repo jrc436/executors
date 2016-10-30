b9f2741deec2b029923f1a9ef6ac33f0a46998c3
Jeremy Cole <jrc436@psu.edu>
2016-10-30 18:08:10 -0400

    added default constructor

--- src/util/data/TextSplitter.java
+++ src/util/data/TextSplitter.java
@@ -3,7 +3,9 @@ package util.data;
 import java.io.File;
 
 public class TextSplitter extends BigDataSplitter<TextList> {
-
+	public TextSplitter() {
+		super();
+	}
 	@Override
 	public TextList getNextData() {
 		File f = super.getNextFile();
