dcacb7892f2b0c29c70863d8f817a1f75a6c4ab2
Jeremy Cole <jrc436@psu.edu>
2016-11-28 16:15:38 -0500

    added some fairly general evaluators for the purpose of optimizers

---
+++ src/optimizers/DoublePair.java
@@ -0,0 +1,16 @@
+package optimizers;
+
+public class DoublePair {
+	private final double first;
+	private final double second;
+	public DoublePair(double first, double second) {
+		this.first = first;
+		this.second = second;
+	}
+	public double getFirst() {
+		return first;
+	}
+	public double getSecond() {
+		return second;
+	}
+}
